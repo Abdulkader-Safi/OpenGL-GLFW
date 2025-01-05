07da5bccf1977784c5fbfbb8dde3384fb94bc549
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2025-01-05 23:18:47 +0300

    make the color of the triangle grad with shaders and fragment

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 <!-- ![img1](/repo/img1.png) -->
 
-![img2](/repo/img2.png)
+<!-- ![img2](/repo/img2.png) -->
+
+![img3](/repo/img3.png)
--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,12 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
 // Vertices coordinates
 GLfloat vertices[] = {
-    -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,    // Lower left corner
-    0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,     // Lower right corner
-    0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,  // Upper corner
-    -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-    0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,  // Inner right
-    0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f      // Inner down
+    -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 0.8f, 0.3f, 0.02f,     // Lower left corner
+    0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 0.8f, 0.3f, 0.02f,      // Lower right corner
+    0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 1.0f, 0.6f, 0.32f,   // Upper corner
+    -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, 0.9f, 0.45f, 0.17f, // Inner left
+    0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, 0.9f, 0.45f, 0.17f,  // Inner right
+    0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f, 0.8f, 0.3f, 0.02f,      // Inner down
 };
 
 // Indices for vertices order
@@ -39,8 +39,8 @@ int main()
   // So that means we only have the modern functions
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  // Create a GLFWwindow object of 800 by 600 pixels, naming it "YoutubeOpenGL"
-  GLFWwindow *window = glfwCreateWindow(800, 600, "YoutubeOpenGL", NULL, NULL);
+  // Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+  GLFWwindow *window = glfwCreateWindow(800, 800, "LearnOpenGL-GLFW", NULL, NULL);
   // Error check if the window fails to create
   if (window == NULL)
   {
@@ -64,7 +64,7 @@ int main()
 
   // Call it once to set the viewport to the correct size
 
-  // Generates Shader object using shaders defualt.vert and default.frag
+  // Generates Shader object using shaders default.vert and default.frag
   Shader shaderProgram("shaders/default.vert", "shaders/default.frag");
 
   // Generates Vertex Array Object and binds it
@@ -77,7 +77,8 @@ int main()
   EBO EBO1(indices, sizeof(indices));
 
   // Links VBO to VAO
-  VAO1.LinkVBO(VBO1, 0);
+  VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void *)0);
+  VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void *)(3 * sizeof(float)));
   // Unbind all to prevent accidentally modifying them
   VAO1.Unbind();
   VBO1.Unbind();
